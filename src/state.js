284c75939e484645e5107b6a734fad75cb1d7ebb
Tarek Adam <tarek.adam@gmail.com>
2020-10-24 10:47:46 -0600

    fix namespace on sta version stores

--- src/state.js
+++ src/state.js
@@ -3,7 +3,7 @@ import Vuex from "vuex";
 import VuexPersist from "vuex-persist";
 
 const vuexPersist = new VuexPersist({
-    key: "x-#stateTagAppVersion#",
+    key: stateTagApp['namespace'].concat("-#stateTagAppVersion#"),
     storage: stateTagApp.disk
 });
 
