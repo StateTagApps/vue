5741abdb4d629356ec19933f2b03cb1f9fd86aa2
Tarek Adam <tarek.adam@gmail.com>
2022-01-10 13:51:22 -0600

    applies services to qr

--- src/x-html/manifest.js
+++ src/x-html/manifest.js
@@ -1,13 +1,13 @@
 import Input from "@/x-html/Input";
-import NebulaQr from "@/x-html/NebulaQr";
+import Qr from "@/x-html/Qr";
 import SocketStatus from "@/x-html/SocketStatus";
 import TextHtml from "@/x-html/TextHtml";
 
 let tags = {
     "input": Input,
-    "nebula-qr": NebulaQr,
     "socket-status": SocketStatus,
     "text-html": TextHtml,
+    "qr": Qr,
 }
 
 const Manifest = Object.assign(
